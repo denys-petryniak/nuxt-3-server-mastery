1a677438b6096e0a99bc162a169cd0ba4d2e6aa7
Denys Petryniak <denys.petryniak@gmail.com>
2024-03-31 07:11:02 +0300

    Add api call to pokeapi

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { data: scizor } = await useFetch(
+  "https://pokeapi.co/api/v2/pokemon/scizor"
+);
+</script>
 
 <template>
   <h1>Page: index</h1>
+  <pre>{{ scizor }}</pre>
 </template>
 
 <style scoped></style>
