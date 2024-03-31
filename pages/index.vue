f6fc4df897d52c8e494fa6283b7cef672c36024a
Denys Petryniak <denys.petryniak@gmail.com>
2024-03-31 07:39:23 +0300

    Add pokemon API

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-const { data: scizor } = await useFetch(
-  "https://pokeapi.co/api/v2/pokemon/scizor"
-);
+const { data: scizor } = await useFetch("/api/scizor");
 </script>
 
 <template>
-  <h1>Page: index</h1>
-  <pre>{{ scizor }}</pre>
+  <h1>Nuxt 3 Server Mastery</h1>
+  <ul>
+    <li>
+      <img :src="scizor.sprite" alt="" />
+    </li>
+    <li>{{ scizor.id }}: {{ scizor.name }}</li>
+  </ul>
 </template>
 
 <style scoped></style>
